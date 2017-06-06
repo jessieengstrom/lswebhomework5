b17ecdb26457a4df6db0d3ee8fe2a33fd854461c
Jessie Engstrom <willisjessie3@jessies-MacBook-Air.local>
2017-06-05 18:20:20 -0700

    fixed errors

--- script.js
+++ script.js
@@ -94,14 +94,14 @@ Hint: Use your last three loops and if/then/else statements. You can learn about
 // FizzBuzz exercise
 
 for (var i = 1; i <= 100; i++) {
-	if (i % 3 === 0) {
-		document.write("Fizz" + "<br>"); 
+	if (i % 3 === 0 && i % 5 === 0) {
+		document.write(i + "FizzBuzz" + "<br>"); 
 	} else if (i % 5 === 0) {
-		document.write("Buzz" + "<br>");
-	} else if (i % 3 && i % 5 === 0) {
-		document.write("FizzBuzz" + "<br>");
+		document.write(i + "Buzz" + "<br>");
+	} else if (i % 3 === 0) {
+		document.write(i + "Fizz" + "<br>");
 	} else {
-		document.write(i+ "<br>");
+		document.write(i + "<br>");
 	}
 }
 
